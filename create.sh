1f777ad48e04ba9457ad8e446c8894fd802b002b
VasyaPupkin120 <VPupkinza@yandex.ru>
2024-06-05 21:18:11 +0700

    add django-stubs

--- create.sh
+++ create.sh
@@ -19,6 +19,7 @@ poetry add gunicorn=^21.2.0
 poetry add psycopg2-binary=^2.9.9
 poetry add environs[django]=^10.3.0
 poetry add django-allauth=^0.61.1
+poetry add django-stubs=^5.0.2 
 # poetry add django-debug-toolbar=^4.3.0
 
 django-admin startproject config .
